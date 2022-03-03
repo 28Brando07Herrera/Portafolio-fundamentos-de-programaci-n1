00519c9bbda03dd5312a393ce12e0e99065c2685
28Brando07Herrera <99735631+28Brando07Herrera@users.noreply.github.com>
2022-03-03 15:14:19 -0500

    Add files via upload

---
+++ IF/ejercicio2.py
@@ -0,0 +1,21 @@
+
+num1=int(input("ingrese num1:"))
+num2=int(input("ingrese num2:"))
+
+if num1>num2:
+   print("El numero mayor es:",num1)
+elif num2>num1:
+    print("El numero mayor es:",num2)
+else:
+    print("Los numeros son iguales")
+
+# Ees
+
+
+
+#if num1>num2:
+    #print("El numero uno es mayor :",num1)
+#if num2>num1:
+    #print("El numero dos es mayor :",num2)
+#if num1==num2:
+    #print("Los numeros son iguales")
---
+++ IF/ejercicio3.py
@@ -0,0 +1,24 @@
+
+
+num1=int(input("ingrese num1:"))
+if num1>0:
+    print("INGRESE OTRO VALOR")
+    num2=int(input("ingrese num2:"))
+    print("suma:",num1+num2)
+    print("resta:",num1-num2)
+    print("producto:",num1*num2)
+else:
+    print("fin")
+print("...")
+
+#Importante
+
+
+#suma=num1+num2
+#print("la suma es:",suma)
+
+#resta=num1-num2
+#print("la resta es:",resta)
+
+#producto= num1*num2
+#print("El producto es:",producto)
